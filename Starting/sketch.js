3c26c80068b784aefe83a6186bcdfbf000867df2
Mike Daniel Ambrocio <36869500+mambrocio@users.noreply.github.com>
2022-03-03 16:23:15 -0500

    Add files via upload
    Starting playground environment for P5.js

---
+++ Starting/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+   createCanvas(800, 400);
+   background(220);
+   rectMode(CENTER);
+}
+
+function draw() {
+	rect(50, 100, 200, 100);
+	rect(400, 150, 100, 100);
+	ellipse(350, 120, 100, 100);
+	line(0, 0, 800, 300);
+}
+
+
