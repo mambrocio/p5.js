3f0347c55524a091cd79b33fd5a64eb4d9e5bb6e
Mike Daniel Ambrocio <36869500+mambrocio@users.noreply.github.com>
2022-03-21 10:07:13 -0400

    Update sketch.js

--- Object/Constructor Functions/sketch.js
+++ Object/Constructor Functions/sketch.js
@@ -1,22 +1,44 @@
+var circle_1;
+var circle_2;
+var circle_3;
+
 function setup(){
-	createCanvas(400, 400);
-	circle = {
-		x: width/2,
-		y: height/2,
-		size: 50,
-		draw: function(){
-			ellipse(this.x, this.y, this.size, this.size );
-		},
-		grow:function(){
-			if (this.size < 200){
-					this.size = this.size + 1;
-				}
-			}
-		}
-	}
+	createCanvas(800, 300);
+	circle_1 =new Circle();
+	circle_2 =new Circle();
+	circle_3 =new Circle();
+}
 
 function draw() {
 	background(220);
-	circle.draw();
-	circle.grow();
+	fill(237, 34, 93);
+	noStroke();
+
+	circle_1.draw();
+	circle_1.grow()
+
+	circle_2.x = 150;
+	circle_2.draw();
+	circle_2.grow()
+
+	circle_3.x = 650;
+	circle_3.draw();
+	circle_3.grow();
+}
+
+
+function Circle (){
+	this.x = width/2;
+	this.y = height/2;
+	this.size = 50;
+
+	this.draw = function(){
+		ellipse (this.x, this.y, this.size, this.size);
+	};
+	this.grow = function(){
+		if(this.size < 200) {
+			this.size = this.size + 1;
+		}
 	}
+
+}
