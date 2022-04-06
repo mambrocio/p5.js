ca897efca0d7b551cca23726894f14ba7e2f31a3
Mike Daniel Ambrocio <36869500+mambrocio@users.noreply.github.com>
2022-04-06 15:14:54 -0400

    Update sketch.js

--- p5 Game/sketch.js
+++ p5 Game/sketch.js
@@ -1,5 +1,5 @@
-var guessItem = '';
-var interval = 69;
+var guessItem;
+var interval = 100;
 
 function setup() {
 	createCanvas(800,300);
@@ -8,11 +8,14 @@ function setup() {
 function draw() {
 	background(220);
 
-	if (frameCount === 1 ||frameCount % interval === 0) {
-		 guessItem = new GuessItem(width/2, height/2, 10);
-	} 
+	if (frameCount === 1 || frameCount % interval === 0) {
+		guessItem = new GuessItem(width/2, height/2, 10);
+	} 	
+	guessItem.render()
+}
 
-	guessItem.render();
+function keyPressed(){
+	console.log('you pressed', key);
 }
 
 function GuessItem(x, y, scl){
@@ -22,14 +25,28 @@ this.scale = scl;
 this.scaleIncrement = 0.5;
 this.content = getContent();
 this.alpha = 255;
-this.alphaDecrement = 200
+this.alphaDecrement = 3;
+this.solved;
 
 function getContent(){
 	return parseInt(random(10), 10);
 }
 
+this.solve = function(input) {
+	if (input === this.content){
+		this.solved = true;
+	} else {
+		this.solved = false;
+	}
+	return
+}
+
 this.render = function(){
+	if (this.solved === false){
+		return;
+	}
 	push();
+	fill(0, this.alpha);
 	textAlign(CENTER, CENTER);
 	translate(this.x, this.y);
 	scale(this.scale);
